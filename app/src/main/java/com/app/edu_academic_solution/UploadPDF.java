7fc61e3911eae55ccd9a412a80d1802097d8b0dd
Samrudhi17 <samrudhipawar15@gmail.com>
2023-05-03 01:57:51 +0530

    connect firebase

--- app/src/main/java/com/app/edu_academic_solution/UploadPDF.java
+++ app/src/main/java/com/app/edu_academic_solution/UploadPDF.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.StorageReference;
 
 public class UploadPDF extends AppCompatActivity {
@@ -13,11 +14,18 @@ public class UploadPDF extends AppCompatActivity {
     Button uploadButton, viewButton;
     EditText pdfName;
 
-
+   StorageReference storageReference;
+   DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_pdf);
+
+        uploadButton = findViewById(R.id.uploadButton);
+        viewButton = findViewById(R.id.viewButton);
+        pdfName = findViewById(R.id.pdfName);
+
+
     }
 }
