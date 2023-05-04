708adb3dc772d7ba00a7d31332d87a007421e89d
Dhananjay <dhananjaypanage11@gmail.com>
2023-05-04 23:47:05 +0530

    Made sure teacher won't be able to see anything that might not be related to them

--- app/src/main/java/com/app/edu_academic_solution/LoginActivity.java
+++ app/src/main/java/com/app/edu_academic_solution/LoginActivity.java
@@ -1,39 +1,55 @@
 package com.app.edu_academic_solution;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Objects;
+
 public class LoginActivity extends AppCompatActivity {
     Spinner spinner;
-    String[] role= {"Student", "Teacher", "Clerk", "Admin"};
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firestore;
+    CollectionReference collectionReference;
+    String currentRole;
+    String[] roles = {"Student", "Teacher", "Clerk", "Admin"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        firestore = FirebaseFirestore.getInstance();
+        firebaseAuth  = FirebaseAuth.getInstance();
+
         spinner = findViewById(R.id.spinner);
-
-
-
-
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(LoginActivity.this,android.R.layout.simple_spinner_item,role);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(LoginActivity.this,android.R.layout.simple_spinner_item, roles);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String value = parent.getItemAtPosition(position).toString();
-                Toast.makeText(LoginActivity.this, "value", Toast.LENGTH_SHORT).show();
+                currentRole = parent.getItemAtPosition(position).toString();
+                Toast.makeText(LoginActivity.this, currentRole, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -41,14 +57,54 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-
-
-
-
     }
 
-    public void loginBtn(View view) {
-        Intent intent = new Intent(LoginActivity.this,HomeScreen.class);
-        startActivity(intent);
+    public void login(View view) {
+        EditText emailEditText = findViewById(R.id.username);
+        EditText passwordEditText = findViewById(R.id.password);
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        // As currentRole is Student, Teacher or Clerk and we need student, teacher or clerk
+        // as these are the collection names
+        currentRole = currentRole.toLowerCase();
+        Log.e("LOGIN", "email: " + email);
+        Log.e("LOGIN", "password: " + password);
+        Log.d("LOGIN", "current role:" + currentRole);
+        collectionReference = firestore.collection(currentRole);
+        Query query = collectionReference.whereEqualTo("email", email);
+
+        query.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                QuerySnapshot querySnapshot = task.getResult();
+                if (querySnapshot.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "No such email with role " + currentRole + " found", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.d("LOGIN", "email: " + email + " found in role: " + currentRole);
+                    Log.d("LOGIN", "Initiating Login Process");
+                    initiateFirebaseLoginProcess(email, password);
+                }
+            }
+            else {
+                Log.e("LOGIN", "There was a issue fetching documents");
+            }
+        });
+    }
+    private void initiateFirebaseLoginProcess(String email, String password) {
+        Log.d("LOGIN", "Logging in with email and password");
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                Log.d("LOGIN", "Login successful");
+                Log.d("LOGIN", "Moving to HomeScreen");
+                FirebaseUser userData = firebaseAuth.getCurrentUser();
+                Intent intent = new Intent(this, HomeScreen.class);
+                intent.putExtra("userData", userData);
+                intent.putExtra("isTeacher", currentRole.equals("teacher"));
+                startActivity(intent);
+            } else {
+                Log.d("LOGIN", "Login failed, password is incorrect");
+                Toast.makeText(LoginActivity.this, "Username or password is incorrect", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
